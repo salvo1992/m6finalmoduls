54196cec6aec93fa02a2c75d9916b282abefadfc
salvo1992 <145794449+salvo1992@users.noreply.github.com>
2024-08-22 19:00:30 +0200

    .

--- fe/src/components/navigationBar/NavigationBar.jsx
+++ fe/src/components/navigationBar/NavigationBar.jsx
@@ -32,7 +32,7 @@ const NavigationBar = () => {
                                     </a>
                                 </li>
                                 <li className="nav-item">
-                                    <a className="nav-link" href="#">
+                                    <a className="nav-link" href="#!">
                                         Contattaci
                                     </a>
                                 </li>
